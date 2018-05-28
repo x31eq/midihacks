89084d7e220790eea7dc2a238ebab7a46c19d76c
Graham Breed <gbreed@gmail.com>
2018-05-28 11:35:42 +0100

    Say the size of everything except SysEx

--- midi.py
+++ midi.py
@@ -1,2 +1,7 @@
 def data_bytes(status):
-    return b'\x02\x02\x02\x02\x01\x01\x02\x00'[status >> 4 & 7]
+    msb = status >> 4
+    assert msb & 8
+    if msb == 0xf:
+        # SysEx is still special
+        return b'?\1\2\1\0\0\0\0\0\0\0\0\0\0\0\0'[status & 0xf]
+    return b'\2\2\2\2\1\1\2'[msb & 7]
--- test.py
+++ test.py
@@ -6,5 +6,9 @@ if __name__ == '__main__':
             (0xb4, 2), (0xc5, 1), (0xd6, 1), (0xe7, 2),
             (0x88, 2), (0x89, 2), (0x8a, 2), (0x8b, 2),
             (0x8c, 2), (0x8d, 2), (0x8e, 2), (0x8f, 2),
+            (0xf1, 1), (0xf2, 2), (0xf3, 1),
+            (0xf4, 0), (0xf5, 0), (0xf6, 0),
+            (0xf8, 0), (0xf9, 0), (0xfa, 0), (0xfb, 0),
+            (0xfc, 0), (0xfd, 0), (0xfe, 0), (0xff, 0),
             ]:
-        assert midi.data_bytes(s) == m
+        assert midi.data_bytes(s) == m, f'Wrong data size for 0x{s:02x}'

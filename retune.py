c42d2020727ec0d5668c2b558355a68e91176ae5
Graham Breed <gbreed@gmail.com>
2018-06-10 18:08:29 +0100

    Cycle over 8 channels

--- retune.py
+++ retune.py
@@ -2,14 +2,16 @@ import midi
 
 class Retuner(midi.stream):
     def hack(self, mess):
+        self.chan = (getattr(self, 'chan', 0) + 1) & 7
+
         if mess[0] & 0xef == 0x80:
             pitch = (mess[1] - 60) * 12 / 29 + 60
             note = round(pitch)
             note = min(max(note, 0), 0x7f)
             if mess[0] == 0x80 and mess[2]:
                 bend = round((pitch - note) * 0x80 * 0x80 / 4) & 0x3fff
-                result = [(0xe0, bend & 0x7f, bend >> 7)]
+                result = [(0xe0 + self.chan, bend & 0x7f, bend >> 7)]
             else:
                 result = []
-            return result + [(mess[0], note, mess[2])]
+            return result + [(mess[0] + self.chan, note, mess[2])]
 
